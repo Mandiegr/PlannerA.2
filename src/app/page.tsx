30fc51a3932cb7af485bf0bcfe4b0b08169c269a
Mandiegr <nannda076@gmail.com>
2024-01-14 23:22:12 -0300

    initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { Main } from './styles'
+//import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <Main>
+      <h1>Agenda</h1>
+    </Main>
+  )
+}
