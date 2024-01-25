422d774b430971b6462134c6aba8a21486714456
Mandiegr <nannda076@gmail.com>
2024-01-25 01:13:41 -0300

    chore: Update project settings and dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['tsx', 'api.ts', 'api.tsx', '/api/*'],
+
+}
 
 module.exports = nextConfig
