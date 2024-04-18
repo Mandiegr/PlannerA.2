2976617113c67c5eafc33d932e0221bba614b3e4
Mandiegr <nannda076@gmail.com>
2024-04-17 23:47:20 -0300

    feat: Moved theme.ts to 'assets/themes' for better organization

---
+++ src/assets/themes/theme.ts
@@ -0,0 +1,34 @@
+
+export const theme = {
+  colors: {
+
+    black: '#000',
+    white: '#FFF',
+
+    rose100:'#F6EAEF',
+    rose200:'#f7e1d7',
+    rose300:'#fae0e4',
+    rose400:'#F2C8C8',
+    rose500:'#edafb8',
+    rose600:'#F9C5DB',
+    rose700:'#F5A1C6',
+    rose800:'#ffc8dd',
+    lilas100: '#cdb4db',
+   
+    green100: '#b0c4b1',
+    green500: '#6b9080',
+    green600: '#588157',
+    green700: '#4a5759',
+    green800: '#1b263b', //#ACCA9C' #03045e
+    
+   
+  },
+  fontSizes: {
+    p: '1rem',
+    md: '1.125rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+    xll: '5rem',
+
+  }
+};
