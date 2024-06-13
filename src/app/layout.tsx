b7d92eb556d1aac09ff01a4ab7e273305600e134
Mandiegr <nannda076@gmail.com>
2024-06-13 02:43:43 -0300

    feat: usercontext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import StyledComponentsRegistry from '@/lib/registry'
 import { ToastContainer } from 'react-toastify'
+import { UserProvider } from '@/context/UserContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,10 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        
+        <UserProvider> 
         <StyledComponentsRegistry> {children} </StyledComponentsRegistry>
         <ToastContainer />
-      
+        </UserProvider>
         </body>
     </html>
   )
